5355c594d377d771ff1fe991684c08c008da491e
hitomi <196425254@qq.com>
2017-01-24 22:25:03 +0800

    Test

--- app/src/main/java/com/hitomi/transferimage/GlideTestActivity.java
+++ app/src/main/java/com/hitomi/transferimage/GlideTestActivity.java
@@ -4,12 +4,20 @@ import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.bumptech.glide.Glide;
+
 public class GlideTestActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_glide_test);
+
+        /**
+         * http://www.jianshu.com/p/548031c62257
+         */
+//        Glide.with(this).using(null, null).load("").as(null).placeholder().load().into()
+
     }
 
     public void popDialog() {

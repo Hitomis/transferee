82673f7b90c7e48ebb27bd150e767b592ab4dfab
hitomi <196425254@qq.com>
2017-01-24 21:13:50 +0800

    Test

---
+++ app/src/main/java/com/hitomi/transferimage/GlideTestActivity.java
@@ -0,0 +1,20 @@
+package com.hitomi.transferimage;
+
+import android.app.Dialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GlideTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_glide_test);
+    }
+
+    public void popDialog() {
+//        Dialog dialog
+
+    }
+
+}

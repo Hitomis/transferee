e4ca6bf8d0d02d670c2111f02c6d2c46f1689a43
Hitomis <196425254@qq.com>
2017-02-03 11:09:16 +0800

    ```

---
+++ tilibrary/src/main/java/com/hitomi/yifangbao/tilibrary/style/index/IndexCircleIndicator.java
@@ -0,0 +1,30 @@
+package com.hitomi.yifangbao.tilibrary.style.index;
+
+import android.support.v4.view.ViewPager;
+import android.view.Gravity;
+import android.widget.FrameLayout;
+
+import com.hitomi.yifangbao.tilibrary.style.IIndexIndicator;
+import com.hitomi.yifangbao.tilibrary.style.view.CircleIndicator;
+
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
+
+/**
+ * Created by hitomi on 2017/2/4.
+ */
+
+public class IndexCircleIndicator implements IIndexIndicator {
+
+    @Override
+    public void getView(FrameLayout parent, ViewPager viewPager) {
+        FrameLayout.LayoutParams indexLp = new FrameLayout.LayoutParams(WRAP_CONTENT, 48);
+        indexLp.gravity = Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL;
+
+        CircleIndicator circleIndicator = new CircleIndicator(parent.getContext());
+        circleIndicator.setViewPager(viewPager);
+        circleIndicator.setGravity(Gravity.CENTER_VERTICAL);
+        circleIndicator.setLayoutParams(indexLp);
+
+        parent.addView(circleIndicator);
+    }
+}

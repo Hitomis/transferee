4a5abb6addb7b631b156fc6d982c57b3a612e644
Hitomis <196425254@qq.com>
2018-02-06 13:17:11 +0800

    simple

--- Glide4Loader/src/main/java/com/wepie/glide4loader/support/OnProgressListener.java
+++
@@ -1,8 +0,0 @@
-package com.wepie.glide4loader.support;
-
-import com.bumptech.glide.load.engine.GlideException;
-
-public interface OnProgressListener {
-
-    void onProgress(String imageUrl, long bytesRead, long totalBytes, boolean isDone, GlideException exception);
-}
